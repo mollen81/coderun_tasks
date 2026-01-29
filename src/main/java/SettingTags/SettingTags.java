f2fd343a257f9390b2e5b39ff8cc75e1f972dc0c
mollen <stepanpanenko@gmail.com>
2026-01-29 22:50:14 +0300

    SettingTags.java

---
+++ src/main/java/SettingTags/SettingTags.java
@@ -0,0 +1,20 @@
+package SettingTags;
+
+import java.io.*;
+
+public class SettingTags
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        /*
+        Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+        int n = Integer.parseInt(reader.readLine());
+        writer.write(String.valueOf(n));
+        */
+
+        reader.close();
+        writer.close();
+    }
+}
