665a3d814d9edb5226f6885ef8ca0920de835221
mollen <stepanpanenko@gmail.com>
2026-02-01 19:46:18 +0300

    first try

--- src/main/java/SymetricalSequence/Main.java
+++ src/main/java/SymetricalSequence/Main.java
@@ -2,6 +2,7 @@ package SymetricalSequence;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,9 +13,9 @@ public class Main
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(reader.readLine());
-        List<Integer> numbers = Arrays.stream(reader.readLine().split(" "))
+        List<Integer> numbers = new ArrayList<>(Arrays.stream(reader.readLine().split(" "))
                 .map(Integer::parseInt)
-                .toList();
+                .toList());
 
         if(n == 1)
         {
@@ -30,8 +31,57 @@ public class Main
         }
         else
         {
+            int middleIndex = 0;
+            int leftIndex = 0;
+            for(int i = 1; i < n - 3; i++)
+            {
+                if(numbers.get(i - 1).equals(numbers.get(i + 3))
+                        && numbers.get(i).equals(numbers.get(i + 2)) )
+                {
+                    middleIndex = i + 1;
+                }
+                else if(numbers.get(i - 1).equals(numbers.get(i + 2))
+                        && numbers.get(i).equals(numbers.get(i + 1)))
+                {
+                    leftIndex = i;
+                }
+            }
 
 
+            if(middleIndex != 0)
+            {
+                if(n - 1 == middleIndex)
+                {
+                    for (int i = middleIndex - 1; i > -1; i--)
+                    {
+                        numbers.add(numbers.get(i));
+                    }
+                }
+                else
+                {
+                    for (int i = n - 1 - middleIndex; i > -1; i--)
+                    {
+                        numbers.add(i);
+                    }
+                }
+            }
+            if(leftIndex != 0)
+            {
+                if(n - 1 == leftIndex)
+                {
+                    for (int i = leftIndex - 1; i > - 1; i--)
+                    {
+                        numbers.add(numbers.get(i));
+                    }
+                }
+                else
+                {
+                    for (int i = n - 1 - leftIndex; i > -1; i--)
+                    {
+                        numbers.add(numbers.get(i));
+                    }
+                }
+            }
         }
 
 
