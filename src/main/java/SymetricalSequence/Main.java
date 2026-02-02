700c8c2bb6a537c19152c0d289eb3a7554e44340
mollen <stepanpanenko@gmail.com>
2026-02-02 16:24:56 +0300

    isPalindrome fix

--- src/main/java/SymetricalSequence/Main.java
+++ src/main/java/SymetricalSequence/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Main
 {
@@ -17,94 +18,22 @@ public class Main
                 .map(Integer::parseInt)
                 .toList());
 
-        List<Integer> extendedList = new ArrayList<>();
 
-        if(n == 1)
-        {
-            writer.write("0");
-        }
-        else if(n == 2)
-        {
-            writer.write("1" + Integer.toString(numbers.getFirst()));
-        }
-        else if(n == 3)
-        {
-            writer.write("2" + numbers.get(n - 2) + numbers.getFirst());
-        }
-        else
-        {
-            int middleIndex = 0;
-            int leftIndex = 0;
-            for(int i = 1; i < n - 3; i++)
-            {
-                if(numbers.get(i - 1).equals(numbers.get(i + 3))
-                        && numbers.get(i).equals(numbers.get(i + 2)) )
-                {
-                    middleIndex = i + 1;
-                }
-                else if(numbers.get(i - 1).equals(numbers.get(i + 2))
-                        && numbers.get(i).equals(numbers.get(i + 1)))
-                {
-                    leftIndex = i;
-                }
-            }
-
-
-            if(middleIndex != 0)
-            {
-                if(n - 1 == middleIndex)
-                {
-                    for (int i = middleIndex - 1; i > -1; i--)
-                    {
-                        numbers.add(numbers.get(i));
-                        extendedList.add(numbers.get(i));
-                    }
-                }
-                else
-                {
-                    for (int i = n - 1 - middleIndex; i > -1; i--)
-                    {
-                        numbers.add(i);
-                        extendedList.add(numbers.get(i));
-                    }
-                }
-            }
-            if(leftIndex != 0)
-            {
-                if(n - 1 == leftIndex)
-                {
-                    for (int i = leftIndex - 1; i > - 1; i--)
-                    {
-                        numbers.add(numbers.get(i));
-                        extendedList.add(numbers.get(i));
-                    }
-                }
-                else
-                {
-                    for (int i = n - 1 - leftIndex; i > -1; i--)
-                    {
-                        numbers.add(numbers.get(i));
-                        extendedList.add(numbers.get(i));
-                    }
-                }
-            }
-        }
-
-        writer.write(extendedList.size() + "\n" + extendedList.toString());
+        System.out.println(isPalindrome(numbers, 0, numbers.size() - 1));
 
         reader.close();
         writer.close();
     }
 
-    public static boolean isPalindrome(@NotNull List<Integer> list)
+    public static boolean isPalindrome(@NotNull List<Integer> list, int start, int end)
     {
-        if(list.size() % 2 == 0)
+        while(start < end)
         {
-            return list.subList(0, list.size() / 2).equals(list.subList(list.size() / 2, list.size()).reversed());
+            if(!Objects.equals(list.get(start), list.get(end))) return false;
+            start++;
+            end--;
         }
-        else
-        {
-            return list.subList(0, list.size() / 2).equals(list.subList(list.size() / 2 + 1, list.size()).reversed());
-        }
+
+        return true;
     }
 }
