a4a4388ab42a47c2797aa9c13dca6e795c315a27
mollen <stepanpanenko@gmail.com>
2026-02-02 16:55:09 +0300

    solution 2

--- src/main/java/SymetricalSequence/Main.java
+++ src/main/java/SymetricalSequence/Main.java
@@ -18,8 +18,32 @@ public class Main
                 .map(Integer::parseInt)
                 .toList());
 
+        if(isPalindrome(numbers, 0, n - 1))
+        {
+            writer.write("0");
+        }
+        else
+        {
+            int palindromeStartIndex = 0;
+            for(int i = 0; i < n; i ++)
+            {
+                if(isPalindrome(numbers, i, n - 1))
+                {
+                    palindromeStartIndex = i;
+                    break;
+                }
+            }
 
-        System.out.println(isPalindrome(numbers, 0, numbers.size() - 1));
+            System.out.println(palindromeStartIndex);
+
+            StringBuilder sb = new StringBuilder();
+            for(int i = palindromeStartIndex - 1; i > -1; i--)
+            {
+                sb.append(numbers.get(i).toString()).append(" ");
+            }
+
+            System.out.println((sb.toString()));
+        }
 
         reader.close();
         writer.close();
