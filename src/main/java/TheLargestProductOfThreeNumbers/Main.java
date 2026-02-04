8c39e01bd6dcb149ec469c507fcefb373f7ac902
mollen <stepanpanenko@gmail.com>
2026-02-04 19:11:18 +0300

    first solution

--- src/main/java/TheLargestProductOfThreeNumbers/Main.java
+++ src/main/java/TheLargestProductOfThreeNumbers/Main.java
@@ -1,15 +1,28 @@
 package TheLargestProductOfThreeNumbers;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<Integer> list = new ArrayList<>()
+        List<Integer> list = new ArrayList<>(Arrays.stream(reader.readLine().split(" "))
+                .map(Integer::parseInt)
+                .toList());
+
+        Collections.sort(list);
+
+        if(list.get(list.size() - 3) * list.get(list.size() - 2) * list.getLast() < list.getFirst() * list.get(1) * list.get(2))
+        {
+            writer.write(list.getFirst() + " " + list.get(1) + " " + list.get(2));
+        }
+        else
+        {
+            writer.write(list.getLast() + " " + list.get(list.size() - 2) + " " + list.get(list.size() - 3));
+        }
+
 
         reader.close();
         writer.close();
