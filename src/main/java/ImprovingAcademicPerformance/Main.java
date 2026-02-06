6bc0c261b1be4a94e1c0a8caa90bed142e555603
mollen <stepanpanenko@gmail.com>
2026-02-06 21:41:45 +0300

    improve academic performance

---
+++ src/main/java/ImprovingAcademicPerformance/Main.java
@@ -0,0 +1,16 @@
+package ImprovingAcademicPerformance;
+
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        reader.close();
+        writer.close();
+    }
+}
