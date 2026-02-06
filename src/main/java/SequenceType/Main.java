0ae7d94f296d83f3b0c91b4056b6369d5165158f
mollen <stepanpanenko@gmail.com>
2026-02-06 14:27:28 +0300

    second try

--- src/main/java/SequenceType/Main.java
+++ src/main/java/SequenceType/Main.java
@@ -1,9 +1,6 @@
 package SequenceType;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Main
 {
@@ -11,84 +8,56 @@ public class Main
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        List<Integer> array = new ArrayList<>();
+        int val1 = Integer.parseInt(reader.readLine().trim());
+        boolean[] result = {true, true, true, true, true, true};
 
         while(true)
         {
-            Integer value = Integer.parseInt(reader.readLine());
-            if(!value.equals(-2000000000))
+            int val2 = Integer.parseInt(reader.readLine().trim());
+            if(val2 == -2000000000) {
+                break;
+            }
+
+            if(val1 == val2)
             {
-                array.add(Integer.parseInt(reader.readLine()));
+                result[1] = false;
+                result[3] = false;
             }
             else
             {
-                break;
+                result[0] = false;
+
+                if(val1 > val2) {
+                    result[2] = false;
+                }
+                if(val1 < val2) {
+                    result[4] = false;
+                }
             }
+
+            val1 = val2;
         }
 
-        boolean constant = true;
-        boolean ascending = true;
-        boolean weaklyAscending = true;
-        boolean descending = true;
-        boolean weaklyDescending = true;
-        boolean random = true;
-        for(int i = 0; i < array.size() - 2; i++)
-        {
-            if(constant && !Objects.equals(array.get(i), array.get(i + 1))) // CONSTANT
-            {
-                constant = false;
-            }
 
-            if(ascending && array.get(i) <= array.get(i + 1)) // ASCENDING
-            {
-                ascending = false;
-            }
-
-            if(weaklyAscending && array.get(i) < array.get(i + 1)) // WEAKLY ASCENDING
-            {
-                weaklyAscending = false;
-            }
-
-            if(descending && array.get(i) >= array.get(i + 1)) // DESCENDING
-            {
-                descending = false;
-            }
-
-            if(weaklyDescending && array.get(i) > array.get(i + 1)) // WEAKLY DESCENDING
-            {
-                weaklyDescending = false;
-            }
-        }
-
-        if(constant || ascending || weaklyAscending || descending || weaklyDescending)
-        {
-            random = false;
-        }
-
-        if(constant)
-        {
+        if(result[0]) {
             writer.write("CONSTANT");
         }
-        else if(ascending)
-        {
+        else if(result[1] && result[2]){
             writer.write("ASCENDING");
         }
-        else if(weaklyAscending)
-        {
+        else if(result[2]){
             writer.write("WEAKLY ASCENDING");
         }
-        else if(descending)
-        {
+        else if(result[3]){
             writer.write("DESCENDING");
         }
-        else if(weaklyDescending)
-        {
+        else if(result[4]){
             writer.write("WEAKLY DESCENDING");
         }
-        else
-        {
+        else {
             writer.write("RANDOM");
         }
+
 
         reader.close();
         writer.close();
