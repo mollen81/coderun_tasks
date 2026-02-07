cca2fce96a119353aed3389ea317e3a8a4a70dd6
mollen <stepanpanenko@gmail.com>
2026-02-07 20:05:51 +0300

    first try

---
+++ src/main/java/TheMinesweeper/Main.java
@@ -0,0 +1,114 @@
+package TheMinesweeper;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = reader.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        int k = Integer.parseInt(input[2]);
+
+        int[][] field = new int[n][m];
+
+        Map<int[], String> bombsMap = new HashMap<>(k);
+
+        for(int i = 0; i < k; i++)
+        {
+            String[] line = reader.readLine().split(" ");
+            int p = Integer.parseInt(line[0]);
+            int q = Integer.parseInt(line[1]);
+            bombsMap.put(new int[]{p - 1, q - 1}, "*");
+
+            addFieldsValues(field, n, m, p, q);
+        }
+
+
+        reader.close();
+        writer.close();
+    }
+
+    public static void addFieldsValues(int[][] field, int n, int m, int p, int q)
+    {
+        if(p - 1 == 0)
+        {
+            if(q - 1 == 0) {
+                for(int i = 0; i < 2; i++) {
+                    for(int j = 0; j < 2; j++){
+                        field[i][j] += 1;
+                    }
+                }
+            }
+            else if(q == m) {
+                for(int i = 0; i < 2; i++) {
+                    for(int j = q - 2; j < q; j++){
+                        field[i][j] += 1;
+                    }
+                }
+            }
+            else
+            {
+                for(int i = 0; i < 2; i++){
+                    for(int j = q - 2; j < q + 1; j++){
+                        field[i][j] += 1;
+                    }
+                }
+            }
+        }
+        else if(p == n)
+        {
+            if(q - 1 == 0){
+                for(int i = p-2; i < p; i++){
+                    for(int j = 0; j < 2; j++) {
+                        field[i][j] += 1;
+                    }
+                }
+            }
+            else if(q == m){
+                for(int i = p-2; i < p; i++){
+                    for(int j = q-2; j < q; j++) {
+                        field[i][j] += 1;
+                    }
+                }
+            }
+            else{
+                for(int i = p-2; i < p; i++){
+                    for(int j = q-2; j < q+1; j++){
+                        field[i][j] += 1;
+                    }
+                }
+            }
+        }
+        else if(q - 1 == 0)
+        {
+            for(int i = p-2; i < p+1; i++){
+                for(int j = 0; j < 2; j++){
+                    field[i][j] += 1;
+                }
+            }
+        }
+        else if(q == m)
+        {
+            for(int i = p-2; i < p+1; i++){
+                for(int j = q-2; j < q; j++){
+                    field[i][j] += 1;
+                }
+            }
+        }
+        else
+        {
+            for(int i = p-2; i < p+1; i++){
+                for(int j = q-2; j < q+1; j++)
+                {
+                    field[i][j] += 1;
+                }
+            }
+        }
+    }
+}
